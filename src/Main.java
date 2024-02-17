ad63a8e80c8e4c8ae3805c532b82ea834a2ad291
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-17 21:57:49 +0900

    feat: Test for MyLinkedList methods

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,20 @@ public class Main {
         myLinkedList.add(4);
         myLinkedList.add(5);
 
+        System.out.print("ADD TO MY LINKED LIST: [");
+        for (Integer i : myLinkedList) {
+            System.out.print(i);
+            System.out.print(", ");
+        }
+        System.out.println("\b\b]");
+
+        System.out.println("GET TO MY LINKED LIST: " + myLinkedList.get(2));
+        System.out.println("GET TO MY LINKED LIST: " + myLinkedList.get(0));
+
+        myLinkedList.remove(0);
         myLinkedList.remove(3);
 
-        System.out.print("MY LINKED LIST: [");
+        System.out.print("REMOVE TO MY LINKED LIST: [");
         for (Integer i : myLinkedList) {
             System.out.print(i);
             System.out.print(", ");
