cfe8585f2f1434414dea45aa75c5307dd83b18b6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-17 21:00:45 +0900

    feat: Test for MyLinkedList methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import linkedlist.MyLinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+        myLinkedList.add(5);
+
+        myLinkedList.remove(0);
+
+        System.out.print("ADD TO MY LINKED LIST: [");
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.print(myLinkedList.get(i));
+            if (i < myLinkedList.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 }
