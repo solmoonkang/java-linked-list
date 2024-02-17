3a878462bb4f20b8434e7123bdc70ece9c634115
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-18 01:11:03 +0900

    feat: Implement Main method for testing Stack and Queue

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import linkedlist.MyLinkedList;
+import stackqueue.MyQueue;
+import stackqueue.MyStack;
 
 public class Main {
     public static void main(String[] args) {
@@ -29,5 +31,27 @@ public class Main {
             System.out.print(", ");
         }
         System.out.println("\b\b]");
+
+
+
+        System.out.println("\n----------\n");
+
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(15);
+        stack.push(18);
+
+        System.out.println("FIRST ELEMENT FROM STACK: " + stack.pop());
+        System.out.println("SECOND ELEMENT FROM STACK: " + stack.pop());
+
+
+
+        System.out.println("\n----------\n");
+
+        MyQueue<Integer> queue = new MyQueue<>();
+        queue.offer(23);
+        queue.offer(72);
+
+        System.out.println("FIRST ELEMENT FROM QUEUE: " + queue.poll());
+        System.out.println("SECOND ELEMENT FROM QUEUE: " + queue.poll());
     }
 }
