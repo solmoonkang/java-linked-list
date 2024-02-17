01dffd97447dcaea9fa098dd76ba5a760841e767
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-17 16:07:17 +0900

    feat: Implement addFirst method

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,10 +1,25 @@
 public class MyLinkedList<T> {
 
-    private MyListNode<T> head;
+    private MyListNode<T> headNode;
+    private MyListNode<T> tailNode;
     private int size;
 
     public MyLinkedList() {
-        head = null;
+        headNode = null;
+        tailNode = null;
         size = 0;
+    }
+
+    public void addFirst(T value) {
+        MyListNode<T> firstNode = headNode;
+        MyListNode<T> newNode = new MyListNode<>(value, firstNode);
+
+        size++;
+
+        headNode = newNode;
+
+        if (firstNode == null) {
+            tailNode = newNode;
+        }
     }
 }
